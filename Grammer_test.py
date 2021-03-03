5a6d7cee15db2bbd519e2dfa331baadd24117853
frodogu <frodo.gu@outlook.com>
2021-03-03 18:21:02 +0800

    Step2. Read selection to be completed

--- Grammer_test.py
+++ Grammer_test.py
@@ -1,7 +1,5 @@
-import openpyxl
-import pandas as pd
+# - List all files in current directory and select one to read from
 import os
-
 def list_files():
     file_data = []
     i=1
@@ -11,19 +9,15 @@ def list_files():
             file_data.append(file)
             i=i+1
     return  file_data
-
 file_data = list_files()
 file_num = int(input("please enter no. of files: "))
-
 file = file_data[file_num-1]
 print(file)
 
+# - Read from selected file and choose selection of range
+import pandas as pd
+sheet = pd.read_excel(file, sheet_name='New Hire')
+sheet = sheet.iloc[2:, :34]
 
-"""
-wb = openpyxl.load_workbook('data.xlsx')
-sheet = wb.get_sheet_by_name('Sheet2')
-range = ['A3':'D20']   #<-- how to specify this?
-spots = pd.DataFrame(sheet.range) #what should be the exact syntax for this?
-
-print (spots)
-"""
+sheet.dropna(subset=["SAP ID(员工的唯一标识)"],inplace=True)
+print(sheet)
